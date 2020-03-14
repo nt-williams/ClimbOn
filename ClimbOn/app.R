8330695cc1b9364d5dcbab62963bd6d807073d7c
Nick Williams <ntwilliams.personal@gmail.com>
2020-03-14 18:21:23 -0400

    v basic tab for bouldering

--- ClimbOn/app.R
+++ ClimbOn/app.R
@@ -30,43 +30,46 @@ make_json <- function(climber, height, grade,
     rdrop2::drop_upload(fileOut, path = "climbing_records")
 }
 
+# TO DO: need a new make json function for boulders
+
 loginPageUI <- function(..., id, title, label = "Sign in") {
-    submitBttn <- f7Button(inputId = "login", label = label)
-    submitBttn[[2]]$attribs$class <- "item-link list-button f7-action-button"
-    submitBttn[[2]]$name <- "a"
-    shiny::tagList(
-        shinyMobile:::f7InputsDeps(),
+  submitBttn <- f7Button(inputId = "login", label = label)
+  submitBttn[[2]]$attribs$class <- "item-link list-button f7-action-button"
+  submitBttn[[2]]$name <- "a"
+  shiny::tagList(
+    shinyMobile:::f7InputsDeps(),
+    shiny::tags$div(
+      id = id,
+      `data-start-open` = "[true]",
+      class = "login-screen",
+      shiny::tags$div(class = "view", shiny::tags$div(
+        class = "page",
         shiny::tags$div(
-            id = id,
-            `data-start-open` = "[true]",
-            class = "login-screen",
-            shiny::tags$div(class = "view", shiny::tags$div(
-                class = "page",
-                shiny::tags$div(
-                    class = "page-content login-screen-content",
-                    shiny::tags$div(class = "login-screen-title",
-                                    title),
-                    shiny::tags$form(
-                        shiny::tags$div(class = "list",
-                                        shiny::tags$ul(
-                                            f7Text(
-                                                inputId = "login_user",
-                                                label = "",
-                                                placeholder = "Username"
-                                            ),
-                                            f7Password(
-                                                inputId = "login_password",
-                                                label = "",
-                                                placeholder = "Password"
-                                            ),
-                                            ...
-                                        )),
-                        shiny::tags$div(class = "list", shiny::tags$ul(shiny::tags$li(submitBttn)))
-                    )
-                )
-            ))
+          class = "page-content login-screen-content",
+          shiny::tags$div(class = "login-screen-title",
+                          title),
+          shiny::tags$form(
+            shiny::tags$div(class = "list",
+                            shiny::tags$ul(
+                              f7Text(
+                                inputId = "login_user",
+                                label = "",
+                                placeholder = "Username"
+                              ),
+                              f7Password(
+                                inputId = "login_password",
+                                label = "",
+                                placeholder = "Password"
+                              ),
+                              ...
+                            )),
+            shiny::tags$div(class = "list", shiny::tags$ul(shiny::tags$li(submitBttn)))
+          )
         )
+      )
+      )
     )
+  )
 }
 
 
@@ -83,56 +86,158 @@ authenticate <- function(user, password) {
 
 # app
 ui <- shinyMobile::f7Page(
-    shinyjs::useShinyjs(),
-    shiny::tags$link(rel = "apple-touch-icon", href = "icons/apple-touch-icon.png"),
-    init = shinyMobile::f7Init(theme = "light"),
-    shinyMobile::f7SingleLayout(
-        navbar = shinyMobile::f7Navbar(
-            title = "If you don't want to fall, you won't...", 
-            hairline = T, 
-            shadow = F
-        ),
-        loginPageUI(id = "loginPage", title = "Get climbing"),
-        shinyjs::hidden(
-            f7BlockHeader(text = textOutput("authentication"))
-        ), 
+  shinyjs::useShinyjs(),
+  shiny::tags$link(rel = "apple-touch-icon", href = "icons/apple-touch-icon.png"),
+  init = shinyMobile::f7Init(theme = "light"),
+  shinyMobile::f7TabLayout(
+    navbar = shinyMobile::f7Navbar(
+      title = "If you don't want to fall, you won't...", 
+      hairline = T, 
+      shadow = F
+    ),
+    loginPageUI(id = "loginPage", title = "Get climbing"),
+    shinyjs::hidden(
+      f7BlockHeader(text = textOutput("authentication"))
+    ), 
+    shinyMobile::f7Tabs(
+      animated = FALSE,
+      swipeable = TRUE,
+      shinyMobile::f7Tab(
+        tabName = "Ropes",
+        icon = f7Icon("tuningfork"),
+        active = TRUE,
         shinyMobile::f7Shadow(
-            intensity = 5, 
-            hover = F, 
-            shinyMobile::f7Card(
-                div(
-                    id = "dataCollect", 
-                    selectInput("climber", "Climber", 
-                                c("Kat", "Nick")),
-                    selectInput("height", "Wall height",
-                                c("9m", "13m")), 
-                    selectInput("grade", "Grade", 
-                                c("5.5", "5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", 
-                                  "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d", 
-                                  "5.13a", "5.13b", "5.13c", "5.13d", "5.14a")), 
-                    selectInput("style", "Ascent style", 
-                                c("Toprope", "Go", "Redpoint", "Onsight", "Flash")), 
-                    conditionalPanel(condition = "input.style == 'Toprope' || input.style == 'Go'", 
-                                     selectInput("takes", "Number of takes", 0:50)),
-                    selectInput("send", "Did you topout?", 
-                                c("Yes", "No")), 
-                    shinyMobile::f7Button(
-                        inputId = "submit",
-                        color = "red", 
-                        label = "Record climb"
-                    )
-                ), 
-                div(
-                    id = "new_climb", 
-                    shinyMobile::f7Button(
-                        inputId = "fuck_ya",
-                        color = "red", 
-                        label = "Add another climb"
-                        )
-                    )
-                ) 
+          intensity = 5, 
+          hover = F, 
+          shinyMobile::f7Card(
+            div(
+              id = "dataCollect", 
+              selectInput("climber", "Climber", 
+                          c("Kat", "Nick")),
+              selectInput("height", "Wall height",
+                          c("9m", "13m")), 
+              selectInput("grade", "Grade", 
+                          c("5.5", "5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", 
+                            "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d", 
+                            "5.13a", "5.13b", "5.13c", "5.13d", "5.14a")), 
+              selectInput("style", "Ascent style", 
+                          c("Toprope", "Go", "Redpoint", "Onsight", "Flash")), 
+              conditionalPanel(condition = "input.style == 'Toprope' || input.style == 'Go'", 
+                               selectInput("takes", "Number of takes", 0:50)),
+              selectInput("send", "Did you topout?", 
+                          c("Yes", "No")), 
+              shinyMobile::f7Button(
+                inputId = "submit",
+                color = "red", 
+                label = "Record climb"
+              )
+            ), 
+            div(
+              id = "new_climb", 
+              shinyMobile::f7Button(
+                inputId = "fuck_ya",
+                color = "red", 
+                label = "Add another climb"
+              )
             )
+          ) 
         )
+      ), 
+      f7Tab(
+        tabName = "Bouldering",
+        icon = f7Icon("tuningfork"),
+        active = FALSE,
+        shinyMobile::f7Shadow(
+          intensity = 5, 
+          hover = F, 
+          shinyMobile::f7Card(
+            div(
+              id = "dataCollect", 
+              selectInput("climber", "Climber", 
+                          c("Kat", "Nick")),
+              selectInput("boulder_grade", "Grade", 
+                          c("V0", "V1", "V2", "V3", "V4", "V5", "V6", 
+                            "V7", "V8", "V9", "V10", "V11", "V12")), 
+              selectInput("style", "Ascent style", 
+                          c("Repetition", "Onsight", "Flash")), 
+              conditionalPanel(condition = "input.style == 'Toprope'", 
+                               selectInput("tries", "Number of attempts", 0:50)),
+              selectInput("send", "Did you topout?", 
+                          c("Yes", "No")), 
+              shinyMobile::f7Button(
+                inputId = "submit",
+                color = "red", 
+                label = "Record climb"
+              )
+            ), 
+            div(
+              id = "new_climb", 
+              shinyMobile::f7Button(
+                inputId = "fuck_ya",
+                color = "red", 
+                label = "Add another climb"
+              )
+            )
+          ) 
+        )
+      )
+    )
+  )
+)
+    
+    
+    
+    
+    
+    
+    
+    # shinyMobile::f7SingleLayout(
+    #     navbar = shinyMobile::f7Navbar(
+    #         title = "If you don't want to fall, you won't...", 
+    #         hairline = T, 
+    #         shadow = F
+    #     ),
+    #     loginPageUI(id = "loginPage", title = "Get climbing"),
+    #     shinyjs::hidden(
+    #         f7BlockHeader(text = textOutput("authentication"))
+    #     ), 
+    #     shinyMobile::f7Shadow(
+    #         intensity = 5, 
+    #         hover = F, 
+    #         shinyMobile::f7Card(
+    #             div(
+    #                 id = "dataCollect", 
+    #                 selectInput("climber", "Climber", 
+    #                             c("Kat", "Nick")),
+    #                 selectInput("height", "Wall height",
+    #                             c("9m", "13m")), 
+    #                 selectInput("grade", "Grade", 
+    #                             c("5.5", "5.6", "5.7", "5.8", "5.9", "5.10a", "5.10b", "5.10c", "5.10d", 
+    #                               "5.11a", "5.11b", "5.11c", "5.11d", "5.12a", "5.12b", "5.12c", "5.12d", 
+    #                               "5.13a", "5.13b", "5.13c", "5.13d", "5.14a")), 
+    #                 selectInput("style", "Ascent style", 
+    #                             c("Toprope", "Go", "Redpoint", "Onsight", "Flash")), 
+    #                 conditionalPanel(condition = "input.style == 'Toprope' || input.style == 'Go'", 
+    #                                  selectInput("takes", "Number of takes", 0:50)),
+    #                 selectInput("send", "Did you topout?", 
+    #                             c("Yes", "No")), 
+    #                 shinyMobile::f7Button(
+    #                     inputId = "submit",
+    #                     color = "red", 
+    #                     label = "Record climb"
+    #                 )
+    #             ), 
+    #             div(
+    #                 id = "new_climb", 
+    #                 shinyMobile::f7Button(
+    #                     inputId = "fuck_ya",
+    #                     color = "red", 
+    #                     label = "Add another climb"
+    #                     )
+    #                 )
+    #             ) 
+    #         )
+    #     )
     )
 
 
